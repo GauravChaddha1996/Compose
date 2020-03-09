0f955f0f13e4c8015772713b0cd5b223584d4d89
Gaurav Chaddha <chaddhag@gmail.com>
2020-03-09 21:51:14 +0530

    Use viper for config

--- main.go
+++ main.go
@@ -5,23 +5,39 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 )
 
 func main() {
+	_loadConfig()
 	db := _openDB()
 	defer db.Close()
 	_initPackages(db)
 	_startServer()
 }
 
+func _loadConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
 func _openDB() *gorm.DB {
 	log.Print("Starting database connection")
-	dbName := "compose"
-	dbParams := "charset=utf8&parseTime=True&loc=Local"
-	dbConnectionConfig := "root:@tcp(127.0.0.1:3306)"
-	db, err := gorm.Open("mysql", dbConnectionConfig+"/"+dbName+"?"+dbParams)
+	dbName := viper.GetString("db.name")
+	dbParams := viper.GetString("db.params")
+	dbConnectionConfig := viper.GetString("db.username") + ":" + viper.GetString("db.password") +
+		"@tcp(" + viper.GetString("db.host") + ":" + viper.GetString("db.port") + ")"
+	dbImplementation := viper.GetString("db.implementation")
+	dbArgs := dbConnectionConfig + "/" + dbName + "?" + dbParams
+	db, err := gorm.Open(dbImplementation, dbArgs)
 	if err != nil {
 		log.Print("Cannot open db")
 		panic(err)
@@ -36,7 +52,8 @@ func _initPackages(db *gorm.DB) {
 
 func _startServer() {
 	log.Print("Starting server at http://localhost:8000")
-	err := http.ListenAndServe(":8000", _getMainRouter())
+	serverPort := ":" + viper.GetString("server.port")
+	err := http.ListenAndServe(serverPort, _getMainRouter())
 	if err != nil {
 		log.Print("Error starting server. Returning")
 		panic(err)
